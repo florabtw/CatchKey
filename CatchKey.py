fc08b66f58ffb07a61e7ab49f70aaaf0dc0f90d2
Ben Sammons <bsammnz@gmail.com>
2014-11-15 13:36:29 -0800

    twilio rendering

--- CatchKey.py
+++ CatchKey.py
@@ -1,21 +1,40 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+questions = [
+'What is your favorite design pattern?',
+'What is your favorite language?'
+]
+
+
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 @app.route('/instructions')
-def receiveCall():
-    return render_template('receiveCall.xml')
+def requestForInstructions():
+    return render_template('BeginCall.xml', company='catchkey')
 
 @app.route('/completed')
 def callCompleted():
     return 'thanks'
 
-@app.route('/recording')
-def receiveRecording():
-    return 'thanks'
+@app.route('/<company>/recording')
+def receiveRecording(company):
+    if not request.args['questionNo']:
+        questionNo = 0
+    else:
+        questionNo = request.args['questionNo'] + 1
+
+    if questionNo >= len(question):
+        return render_template( 'Hangup.xml' )
+
+    question = questions[ questionNo ]
+    render_template( 'Question.xml',
+     company=company,
+     question=question,
+     questionNo=questionNo )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
