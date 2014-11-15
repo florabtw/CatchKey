f4098e714afc90244dfc8f8bb559161aae8eedf9
Nick Pierson <nkcrpn@gmail.com>
2014-11-15 12:53:05 -0600

    Adds receive call

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/instructions')
+def receiveCall():
+    return render_template('receive_call.xml')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
