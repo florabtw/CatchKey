8aa59a3ef3ae7020bf96b3ada089b5fc4066db0e
Ben Sammons <bsammnz@gmail.com>
2014-11-15 18:51:38 -0800

    updated question setting

--- CatchKey.js
+++ CatchKey.js
@@ -49,7 +49,7 @@ app.post('/:company/questions', function( request, response) {
       if (key.indexOf('question') >= 0) {
         var num = key.substring( 'question'.length );
         var answers = request.body[ 'answer'+num ];
-        var questions = request.body[ 'question'+num ];
+        var question = request.body[ 'question'+num ];
         questions.push( {'question': question, 'answers': answers.split(',') } )
       }
     }
