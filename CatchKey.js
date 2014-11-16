cede6ef042a08369cced413e564e0b337a760f26
Ben Sammons <bsammnz@gmail.com>
2014-11-15 23:04:25 -0800

    here we go clarify

--- CatchKey.js
+++ CatchKey.js
@@ -98,7 +98,7 @@ function analyzeCandidate(company, candidatePhoneNumber) {
     setTimeout(function() {
       client.search({
         'query' : 'the OR fat OR cat OR lazy',
-        'filter' : 'bundle.name==\"'+can[i].answer + '\"'
+        'query_filter' : 'bundle.name==\"'+can[i].answer + '\"'
       },function() {
         console.log(arguments)
       })
